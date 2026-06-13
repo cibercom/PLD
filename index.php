a4bf1821533933e11a5d650b5016912c4a03f903
cibercom <osvaldito.almonte.2030@gmail.com>
2026-06-13 14:36:03 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+    <head>
+       	<meta charset="UTF-8">
+        <meta name="viewport" id="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
+        <title> PLD </title>
+<style>
+  body {
+  background-image: url('pld.jpeg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-attachment: fixed; 
+  margin: ;
+  height: 100vh;
+    background-color: #0FFE1D;
+  }
+.diamond-button {
+    width: 150px;
+    height: 150px;
+    background-color: #E3AB46;
+    transform: rotate(45deg);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: none;
+    cursor: pointer;
+    text-decoration: none;
+    color: white;
+    font-weight: bold;
+  }
+
+  .diamond-button span {
+    transform: rotate(-45deg); /* Endereza el texto */
+  }
+.diamondsecond-button {
+    width: 300px;
+    height: 60px;
+    background-color: #00BB2D;
+    transform: rotate(0deg);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: none;
+    cursor: pointer;
+    text-decoration: none;
+    color: white;
+    font-weight: bold;
+  }
+
+  .diamond-button span {
+    transform: rotate(-0deg); /* Endereza el texto */
+  }
+
+</style>
+    </head>
+    <body>
+
+    <?php
+        echo "<h2 style=color:gold>Partido Liberacion Dominicana</h2><br><br><br><br>";
+    ?>
+	        <center><button class="diamond-button"><a href="https://pld.org.do/"><strong><h1 style="color:gold"><img src="logo.png" width="150" height="150">Registrate</h1><strong></a></button></center><br><br><br><br><br>
+
+       </body>
+</html>		
